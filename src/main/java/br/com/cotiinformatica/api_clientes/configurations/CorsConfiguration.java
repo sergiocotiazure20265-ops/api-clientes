4c5ea8950bac367d523857a0b70132777eabbd95
sergiocotiazure20265-ops <sergiocotiazure20265@gmail.com>
2026-08-14 12:58:11 -0300

    feat: adiciona a configuração do CORS na Api.

---
+++ src/main/java/br/com/cotiinformatica/api_clientes/configurations/CorsConfiguration.java
@@ -0,0 +1,22 @@
+package br.com.cotiinformatica.api_clientes.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        //Permissão para o projeto Angular
+        registry
+                .addMapping("/**") //permissão para todos os ENDPOINTS
+                .allowedOrigins("http://localhost:4200") //projeto Angular
+                .allowedMethods("POST", "PUT", "DELETE", "GET")
+                .allowedHeaders("*");
+    }
+}
